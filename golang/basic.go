2a69a535c569c8a2dd52f90539edaeb9c24c720b
starrynightdream <1023680139@qq.com>
2022-01-02 23:57:49 +0800

    in chan

--- golang/basic.go
+++ golang/basic.go
@@ -27,7 +27,7 @@ func type_switch() {
 	}
 }
 
-// use like
+	// use like
 	// sq := seq()
 	// fmt.Println(sq())
 	// fmt.Println(sq())
@@ -51,7 +51,7 @@ func (person Person) sayhello() string {
 	return ans
 }
 
-// use like
+	// use like
 	// p := Person{name:"Jack"}
 	// p.sayhello()
 
@@ -64,6 +64,116 @@ func stringsTest() {
 	fmt.Println(strings.Join(listofstring, " "))
 }
 
+func slice_test() {
+	numbers := []int {1, 1, 2, 3, 5, 8, 13}
+	numbers_temp := append(numbers, 21, 34)
+
+	show_slice(numbers)
+	show_slice(numbers_temp)
+
+	numbers_test := numbers_temp[2:5]
+
+	show_slice(numbers_test)
+}
+
+func show_slice(sli []int) {
+	fmt.Printf("size: %d cap: %d \n", len(sli), cap(sli))
+	fmt.Println(sli)
+}
+
+func use_range() {
+	numbers := [10]int {0,1,2,3,4,5,6,7,8,9}
+
+	for i := range numbers {
+		fmt.Println(i)
+	}
+}
+
+func use_map() {
+	var mymap map[string] string
+	mymap = map[string] string {"chino":"coffee", "cocoa":"cocos", "molika":"moseka"}
+
+	show_map(mymap)
+
+	delete(mymap, "molika")
+
+	show_map(mymap)
+}
+func show_map(m map[string] string) {
+	fmt.Printf("info: size: %d \n", len(m))
+	for k := range m {
+		fmt.Println("key: ", k, " val: ", m[k])
+	}
+}
+
+// interface
+	// if i miss a func in one struct, i can't use it with this interface
+
+	// use like
+	// aki := Otaku{name:"**##!", love:"vetoreka"}
+	// nanami := Component{belong:"last standdust"}
+
+	// join_work(aki)
+	// join_work(nanami)
+type Speaker interface {
+	success()
+	speak(word string)
+}
+
+type Otaku struct {
+	name, love string
+}
+
+type Component struct {
+	belong string
+}
+
+func (otaku Otaku) success() {
+	fmt.Println("my name is ", otaku.name," I love ", otaku.love)
+}
+func (otaku Otaku) speak(word string) {
+	fmt.Println("(<ゝω·)☆: ", word)
+}
+
+func (comp Component) success() {
+	fmt.Println("think to ", comp.belong)
+}
+func (comp Component) speak(word string) {
+	fmt.Println("working and say: ", word)
+}
+
+func join_work(speaker Speaker) {
+	speaker.speak("HEhe")
+	speaker.success()
+}
+// end interface
+
+// goroutine
+// todo: finish logical
+func personA(web chan string, receive <-chan bool) {
+	for <-receive{
+	}
+}
+func personB(web chan string, receive <-chan bool) {
+	for <-receive{
+	}
+}
+func personC(web chan string, receive <-chan bool) {
+	for <-receive{
+	}
+}
+
+func center_check(from <-chan string, to chan<- string, toA,toB,toC chan<- bool) {
+
+}
+
+func run_time() {
+	web := make(chan string)
+	flagA, flagB, flagC := make(chan string)
+}
+// end goroutine
+
+
 func main() {
 
 	/*
@@ -74,7 +184,4 @@ func main() {
 	fmt.Println("we can find ++b", testbb)
 	*/
 	// with err, b++ can't as expressment
-
-	stringsTest()
-
 }
